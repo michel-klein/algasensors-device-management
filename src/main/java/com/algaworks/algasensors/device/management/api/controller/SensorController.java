9702c00b37f68d0225be94ff0e7eda98431ae975
Michel Klein <michel.kps@outlook.com>
2025-12-16 17:02:40 -0300

    feat: sensor persistence

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -1,26 +1,49 @@
 package com.algaworks.algasensors.device.management.api.controller;
 
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
+import com.algaworks.algasensors.device.management.api.model.SensorOutput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/sensors")
+@RequiredArgsConstructor
 public class SensorController {
+
+    private final SensorRepository sensorRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Sensor create(@RequestBody SensorInput input) {
-        return Sensor.builder()
-                .id(IdGenerator.generateTSID())
+    public SensorOutput create(@RequestBody SensorInput input) {
+        Sensor sensor = Sensor.builder()
+                .id(new SensorId(IdGenerator.generateTSID()))
                 .name(input.getName())
                 .ip(input.getIp())
                 .location(input.getLocation())
                 .protocol(input.getProtocol())
                 .model(input.getModel())
                 .enabled(false)
+                .build();
+
+        sensor = sensorRepository.saveAndFlush(sensor);
+
+        return convertToModel(sensor);
+    }
+
+    private SensorOutput convertToModel(Sensor sensor) {
+        return SensorOutput.builder()
+                .id(sensor.getId().getValue())
+                .name(sensor.getName())
+                .ip(sensor.getIp())
+                .location(sensor.getLocation())
+                .protocol(sensor.getProtocol())
+                .model(sensor.getModel())
+                .enabled(sensor.getEnabled())
                 .build();
     }
 }
